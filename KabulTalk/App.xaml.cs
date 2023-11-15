a733d283fa057426f4d77fb8356a20b82a8fea49
John <kimo87@gmail.com>
2023-11-15 21:14:36 +0900

    프로젝트 생성
    프로젝트생성

---
+++ KabulTalk/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿
+using KabulTalk.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KabulTalk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            var mainView = App.Current.Services.GetService<MainView>();
+            mainView.Show();
+        }
+
+        public new static App Current => (App)Application.Current;
+
+        public IServiceProvider Services { get; }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            //Services
+            //services.AddSingleton<ITestService,TestService>();
+
+            //Views
+            services.AddSingleton<MainView>();
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ WpfLib/Extensions/FindParentExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfLib.Extensions
+{
+    public static class FindParentExtension
+    {
+        public static T? FindParent<T>(this DependencyObject chiled)
+        where T : DependencyObject
+        {
+            return FindParent<T>(chiled, null);
+        }
+
+        public static T? FindParent<T>(this DependencyObject chiled, string? parentName)
+        where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(chiled);
+            if (parent == null)
+            {
+                return null;
+            } 
+
+            var freameworkElement = (FrameworkElement)parent;
+            if ((parentName == null || freameworkElement.Name == parentName) && freameworkElement is T)
+            {
+                return (T)parent;
+            }
+            else
+            {
+                return FindParent<T>(parent, parentName);
+            }
+
+        }
+    }
+}
